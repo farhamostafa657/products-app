b550f166b1242accaecbc76ff6d3de715bb83959
Your Name <farhamostafa657@gmail.com>
2025-02-03 18:07:58 +0400

    add routing

--- products-app/src/app/app.component.ts
+++ products-app/src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { DetailsComponent } from './details/details.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ProductListComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ProductListComponent,
+    DetailsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- products-app/src/app/app.routes.ts
+++ products-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CartComponent } from './cart/cart.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'products', component: ProductListComponent, title: 'Products' },
+  { path: 'login', component: LoginComponent, title: 'Login' },
+  { path: 'register', component: RegisterComponent, title: 'Register' },
+  { path: 'cart', component: CartComponent, title: 'Cart' },
+  { path: '**', component: NotFoundComponent, title: 'Not Found Page' },
+];
