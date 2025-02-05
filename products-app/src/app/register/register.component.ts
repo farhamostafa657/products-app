5cf0d823cdb6100c0099ac862b7632f34fff1b0a
Your Name <farhamostafa657@gmail.com>
2025-02-05 10:52:05 +0400

    register validation using reactive forms

--- products-app/src/app/register/register.component.ts
+++ products-app/src/app/register/register.component.ts
@@ -1,10 +1,81 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  submitted: boolean = false;
+  registForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.registForm = this.fb.group(
+      {
+        registerName: ['', Validators.required],
+        registerEmail: [
+          '',
+          Validators.required,
+          Validators.pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/),
+        ],
+        registerUserName: [
+          '',
+
+          [Validators.required, Validators.pattern(/^[a-zA-Z0-9_]+$/)],
+        ],
+        registerPassword: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(
+              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@%$#*]).{8,}$/
+            ),
+          ],
+        ],
+        registerConfirmPassword: ['', Validators.required],
+      },
+      {
+        validator: confirmMatchedPasswords(
+          'registerPassword',
+          'registerConfirmPassword'
+        ),
+      }
+    );
+  }
+  get formControl() {
+    return this.registForm.controls;
+  }
+
+  handleSubmitForm() {
+    this.submitted = true;
+  }
+}
+
+export function confirmMatchedPasswords(
+  passwordKey: string,
+  confirmPasswordKey: string
+): ValidatorFn {
+  return (formGroup: AbstractControl): ValidationErrors | null => {
+    const password = formGroup.get(passwordKey)?.value;
+    const confirmPassword = formGroup.get(confirmPasswordKey)?.value;
+    if (password != confirmPassword) {
+      return { passwordMisMatch: true };
+    }
+    return null;
+  };
+}
+
+// TS => Import ReactiveFormsModule
+// TS => init. FormGroup = ({ formControl('', [Validators])  })
+// HTML => <form [formGroup]="form" (ngSubmit)="">
+// HTML => <field formControlName="formControl">
